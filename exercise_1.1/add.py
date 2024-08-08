56868b119438e62c3f569ed9a9fe98aa067fc02a
RParker505 <147425968+RParker505@users.noreply.github.com>
2024-08-08 06:05:44 -0600

    Upload add.py script

---
+++ exercise_1.1/add.py
@@ -0,0 +1,11 @@
+# prompt user for first number, assign input to variable a
+a = int(input("Enter any number: "))
+
+# prompt user for a second number, assign input to variable b
+b = int(input("Enter another number: "))
+
+# add variables a and b and store the sum in variable c
+c = a + b
+
+# print c
+print("The total of", a, "+", b, "is:", c)
